ccbdde8744c17461d06715fecf8a1dab7a0b3c43
sagar.sonara.cognisun@gmail.com <sagar.sonara.cognisun@gmail.com>
2025-09-24 10:07:47 +0530

    Basic Updates on patch

--- common_helper/patch.py
+++ common_helper/patch.py
@@ -1,9 +1,8 @@
-import requests, ssl
+import requests
+import ssl
 from requests.adapters import HTTPAdapter
-from urllib3.poolmanager import PoolManager
 
 class TLSAdapter(HTTPAdapter):
-    """Custom adapter to allow TLSv1.2+ with lower SECLEVEL"""
     def init_poolmanager(self, *args, **kwargs):
         ctx = ssl.create_default_context()
         ctx.minimum_version = ssl.TLSVersion.TLSv1_2
@@ -11,13 +10,14 @@ class TLSAdapter(HTTPAdapter):
         kwargs['ssl_context'] = ctx
         return super().init_poolmanager(*args, **kwargs)
 
-# Keep original Session init
-_session_init = requests.Session.__init__
-
-def _patched_init(self, *args, **kwargs):
-    _session_init(self, *args, **kwargs)
-    self.mount("https://", TLSAdapter())
-
 def apply_patch():
-    """Patch requests globally so all sessions use TLSAdapter"""
-    requests.Session.__init__ = _patched_init
+    _session_init = requests.Session.__init__
+
+    def _patched_init(self, *args, **kwargs):
+        _session_init(self, *args, **kwargs)
+        self.mount("https://", TLSAdapter())
+
+    # Only patch once
+    if getattr(requests.Session, "_patched_tls", False) is False:
+        requests.Session.__init__ = _patched_init
+        requests.Session._patched_tls = True
